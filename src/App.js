4e8080a9862e7b88b8d235ce610fd438687be280
mahan-sepehri <mahan.sepehrifar@gmail.com>
2022-01-25 02:29:50 +0330

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import Row from "./components/Row";
+
+const App = () => {
+  const [activeRow, setActiveRow] = useState(1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Row
+        active={activeRow === 1 ? true : false}
+        setActiveRow={setActiveRow}
+      />
+      <Row
+        active={activeRow === 2 ? true : false}
+        setActiveRow={setActiveRow}
+      />
+      <Row
+        active={activeRow === 3 ? true : false}
+        setActiveRow={setActiveRow}
+      />
+      <Row
+        active={activeRow === 4 ? true : false}
+        setActiveRow={setActiveRow}
+      />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Row.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useRef } from "react";
+import "./Row.css";
+import dictionary from "../dictionary/dictionary";
+
+let activeTile = 0;
+const randomNum = Math.floor(Math.random() * dictionary.length);
+let answer = dictionary[randomNum];
+const Row = (props) => {
+  const tileRef1 = useRef();
+  const tileRef2 = useRef();
+  const tileRef3 = useRef();
+  const tileRef4 = useRef();
+  const tileRef5 = useRef();
+  const tiles = [tileRef1, tileRef2, tileRef3, tileRef4, tileRef5];
+
+  const checkGuess = (guess) => {
+    if (guess.join("") === answer) {
+      tiles.forEach((tile) => tile.current.classList.add("green"));
+      return console.log("you win");
+    }
+    let remainingGuess = [];
+    let remainingAnswer = [];
+    let guessColor = [null, null, null, null, null];
+    for (let i = 0; i < guess.length; i++) {
+      if (guess[i] === answer[i]) {
+        tiles[i].current.classList.add("green");
+        guessColor[i] = "green";
+      } else {
+        remainingGuess.push(guess[i]);
+        remainingAnswer.push(answer[i]);
+      }
+    }
+
+    for (let i = 0; i < guessColor.length; i++) {
+      if (
+        !guessColor[i] &&
+        remainingGuess.length > 0 &&
+        remainingAnswer.includes(remainingGuess[0])
+      ) {
+        tiles[i].current.classList.add("yellow");
+        guessColor[i] = "yellow";
+        remainingAnswer.splice(remainingAnswer.indexOf(remainingGuess[0]), 1);
+        remainingGuess.splice(0, 1);
+      } else if (!guessColor[i]) {
+        tiles[i].current.classList.add("grey");
+
+        guessColor[i] = "grey";
+        remainingGuess.splice(0, 1);
+      }
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    const key = e.key.toLowerCase();
+    const isLetter = key >= "ا" && key <= "ی";
+
+    if (e.key === "Backspace") {
+      if (activeTile === 0) {
+        return;
+      }
+      activeTile--;
+      tiles[activeTile].current.textContent = "";
+      return;
+    }
+    if (activeTile > 4 && e.key !== "Enter") {
+      return console.log(activeTile, "no more");
+    } else if (activeTile > 4 && e.key === "Enter") {
+      const guessed = tiles.map((tile) => tile.current.textContent);
+      document.removeEventListener("keydown", handleKeyDown);
+      checkGuess(guessed);
+      props.setActiveRow((current) => current + 1);
+      activeTile = 0;
+      return;
+    }
+    if (key.length === 1 && isLetter) {
+      tiles[activeTile].current.textContent = e.key;
+      activeTile++;
+    }
+  };
+
+  useEffect(() => {
+    if (props.active) {
+      document.addEventListener("keydown", handleKeyDown);
+    }
+  });
+
+  return (
+    <div className="row">
+      <div className="tile first" ref={props.active ? tileRef1 : null}></div>
+      <div className="tile" ref={props.active ? tileRef2 : null}></div>
+      <div className="tile" ref={props.active ? tileRef3 : null}></div>
+      <div className="tile" ref={props.active ? tileRef4 : null}></div>
+      <div className="tile" ref={props.active ? tileRef5 : null}></div>
+    </div>
+  );
+};
+
+export default Row;
